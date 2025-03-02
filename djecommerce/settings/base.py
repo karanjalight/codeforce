59c9e7bb87658d6f41ee969876ac72beb313b855
root <karanjalight>
2025-03-02 21:37:34 +0000

    hiding the secrets

--- djecommerce/settings/base.py
+++ djecommerce/settings/base.py
@@ -1,14 +1,14 @@
 import os
-from decouple import config
+from decouple import config  # Ensure python-decouple is installed
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-BASE_DIR = os.path.dirname(os.path.dirname(
-    os.path.dirname(os.path.abspath(__file__))))
+SECRET_KEY = config("DJANGO_SECRET_KEY", default="your-default-secret-key")
 
-SECRET_KEY =  'codeforce'
+DEBUG = config("DEBUG", default=True, cast=bool)
 
-DEBUG = True
 ALLOWED_HOSTS = ['*', 'codeforce.co.ke', 'www.codeforce.co.ke']
+
 INSTALLED_APPS = [
     'jazzmin',
     'django.contrib.admin',
@@ -17,10 +17,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'django.contrib.sites',
-    # 'allauth',
-    # 'allauth.account',
-    # 'allauth.socialaccount',
     'crispy_forms',
     'django_countries',
     'crispy_bootstrap4',
@@ -28,9 +24,8 @@ INSTALLED_APPS = [
 
     # Rest Framework
     'rest_framework',
-    # 'django_daraja',
 
-    #apps
+    # Apps
     'core',
     'accounts',
     'whitenoise.runserver_nostatic',    
@@ -45,11 +40,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    # Downloaded Middleware
-# 'allauth.account.middleware.AccountMiddleware',
 ]
-
-
 
 ROOT_URLCONF = 'djecommerce.urls'
 
@@ -69,7 +60,6 @@ TEMPLATES = [
     },
 ]
 
-
 WSGI_APPLICATION = 'djecommerce.wsgi.application'
 
 LANGUAGE_CODE = 'en-us'
@@ -78,76 +68,35 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-
-# Users model
 AUTH_USER_MODEL = "accounts.CustomUser"
 
-# Rest Framework
 REST_FRAMEWORK = {
    'DEFAULT_AUTHENTICATION_CLASSES': [
        'rest_framework_simplejwt.authentication.JWTAuthentication',
    ],
 }
 
-# Static files (CSS, JavaScript, Images)
-
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-# Absolute path where static files will be collected
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static_in_env"),
-]
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static_in_env")]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# Auth
-
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
-    # 'allauth.account.auth_backends.AuthenticationBackend'
 )
+
 SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
-
-# CRISPY FORMS
-
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-
-
-INSTALLED_APPS += [
-    'debug_toolbar'
-]
-
-MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware', ]
-
-# DEBUG TOOLBAR SETTINGS
-
-DEBUG_TOOLBAR_PANELS = [
-    'debug_toolbar.panels.versions.VersionsPanel',
-    'debug_toolbar.panels.timer.TimerPanel',
-    'debug_toolbar.panels.settings.SettingsPanel',
-    'debug_toolbar.panels.headers.HeadersPanel',
-    'debug_toolbar.panels.request.RequestPanel',
-    'debug_toolbar.panels.sql.SQLPanel',
-    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
-    'debug_toolbar.panels.templates.TemplatesPanel',
-    'debug_toolbar.panels.cache.CachePanel',
-    'debug_toolbar.panels.signals.SignalsPanel',
-    'debug_toolbar.panels.logging.LoggingPanel',
-    'debug_toolbar.panels.redirects.RedirectsPanel',
-]
-
-
-def show_toolbar(request):
-    return False
-
+INSTALLED_APPS += ['debug_toolbar']
+MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware']
 
 DEBUG_TOOLBAR_CONFIG = {
     'INTERCEPT_REDIRECTS': False,
-    'SHOW_TOOLBAR_CALLBACK': show_toolbar
+    'SHOW_TOOLBAR_CALLBACK': lambda request: False,
 }
 
 DATABASES = {
@@ -157,37 +106,11 @@ DATABASES = {
    }
 }
 
-
-# DATABASES = {
-#      'default': {
-#          'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#          'NAME': 'myproject',
-#          'USER': 'myprojectuser',
-#          'PASSWORD': 'password',
-#          'HOST': '127.0.0.1',
-#          'PORT': '5432',
-#      }
-#  }
-
-
-# DATABASES = {
-#      'default': {
-#          'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#          'NAME': 'myproject',
-#          'USER': 'myprojectuser',
-#          'PASSWORD': 'password',
-#          'HOST': '127.0.0.1',
-#          'PORT': '5432',
-#      }
-#  }
-
-
-# SENDGRID_API_KEY="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"
-
+# Email Configuration (Use Environment Variables)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'itisha.delivery@gmail.com'
-EMAIL_HOST_PASSWORD = 'hmghsoyhzclgsuyh'
+EMAIL_HOST_USER = config("EMAIL_HOST_USER", default="")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", default="")
 
