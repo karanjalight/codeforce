4bf6281d09a16d036f399a51bf436da3c72902c9
karanjalight <karanjalight@gmail.com>
2025-03-08 13:58:55 +0300

    roberts changers reviewed - urls

--- core/urls.py
+++ core/urls.py
@@ -26,4 +26,33 @@ urlpatterns = [
     path('settings/',  settings_view, name='candidates'),
 
     
+    
+    
+    
+    path('pay-per-hire/', pay_per_hire_view, name='landing'),
+    path('payment/pay-per-hire/', pay_per_hire_payment_view, name='pay-per-hire-payment'),
+    
+    path('pay-on-demand/', pay_on_demand_view, name='landing'),
+    path('payment/pay-on-demand/', pay_on_demand_payment_view, name='pay-per-hire-payment'),
+
+    path('pay-pro/', pay_pro_view, name='pay-pro-landing'),
+    path('payment/pay-pro/', pay_pro_payment_view, name='pay-pro'),
+
+    
+    # post
+    path('post-pay-per-hire/', dashboard_view, name='submit_job'),
+    
+    
+    
+    
+    path('teachers/', teacher_view, name='teachers'),
+    path('events/', events_view, name='events'),
+    path('contacts/', contact_view, name='contact'),
+    path('school-fees/', fees_view, name='events'),
+    path('dashboard/', dashboard_view, name='dashboard'),
+    path('payment/', payment_view, name='payment'),
+    path('success/<ref>/<amt>/', verify_payment, name='verify-payment'),
+    path('payment/history', payment_history_view, name='payment-history'),
+
+    
 ]
